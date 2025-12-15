7c824dcacbf747e14c00045f7a0e4ee35cab5891
up202306340 <up202306340@up.pt>
2025-12-15 16:31:27 +0000

    final touches

--- Lab 2- Computer Network Configuration/download.h
+++ Lab 2- Computer Network Configuration/download.h
@@ -11,6 +11,17 @@ typedef struct URL {
 
 #define FTP_PORT 21
 
+#define CMD_SIZE 128
+
+#define CONNECTION_OPEN_CODE "125"
+#define OPENING_CONNECTION_CODE "150"
+#define SERVICE_READY_CODE "220"
+#define QUIT_SUCCESS_CODE "221"
+#define TRANSFER_COMPLETE_CODE "226"
+#define PASV_MODE_CODE "227"
+#define LOGIN_SUCCESS_CODE "230"
+#define USERNAME_OK_CODE "331"
+
 int parse(char *link, URL *url);
 int createsocket(const char *adr, int port);
 
